8d1fc965ef7ce59c310e0d366dc3df24763fe620
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-03-31 09:59:57 +0530

    Array Pass By Reference

---
+++ arrays/simple.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class simple
+{
+     public static void update(int arr[])
+        {
+            int sum=0;
+            for(int i=0;i<arr.length;i++)
+            {
+                 arr[i] = arr[i]+1;
+            }
+        }
+    public static void main(String args[])
+        {
+    
+        int arr[]={10,20,29};
+        update(arr);
+        System.out.println("Array Values :");
+
+            for(int i=0;i<arr.length;i++)
+            {
+                System.out.println(arr[i]+" ");
+            }
+        }
+}
