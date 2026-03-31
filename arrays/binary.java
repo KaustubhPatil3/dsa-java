5de0444c2782891136c201b21febe6f9df55a918
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-03-31 11:02:11 +0530

    Binary Search

---
+++ arrays/binary.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class binary
+{
+    public static int binary(int arr[],int key)
+    {
+        int start=0,end=arr.length-1;
+        while (start<=end) 
+        {
+            int mid=(start+end)/2;
+
+            if(arr[mid]==key)
+            {
+                return mid;
+            }
+            if(arr[mid]<key)
+            {
+                start=mid+1;
+            }
+            if(arr[mid]>key)
+            {
+                end=mid-1;
+            }
+        }
+     return -1;
+    }
+    public static void main(String args[])
+    {
+        int arr[]={10,12,14,18,22};
+        int key=18;
+        int i=binary(arr,key);
+        if(i==-1)
+        {
+            System.out.println("not found");
+        }
+        else
+        {
+        System.out.println("Key= "+key+" Found at position= "+i);
+        }
+
+    }
+    
+}
