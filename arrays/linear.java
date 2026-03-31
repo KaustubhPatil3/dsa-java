debdffc643975e7f5b04a4940141342145392961
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-03-31 10:46:00 +0530

    Array linear search

---
+++ arrays/linear.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class linear 
+{
+    public static int linear(int arr[],int num)
+    {
+        int n=num;
+
+        for (int i=0;i<arr.length;i++)
+        {
+            if (arr[i]==n)
+            {
+               return i; 
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String args[])
+    {
+        int arr[]={2,5,3,8,7,1};
+        int num=7;
+        int i=linear(arr,num);
+        if(i==-1)
+        {
+            System.out.println("Not Found");
+        }
+        else
+        {
+            System.out.println("Number found at="+i);
+        }
+    }
+}
