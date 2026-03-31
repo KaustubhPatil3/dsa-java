3049840c3678a5775b63ea3b981471bc312f0432
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-03-31 19:43:09 +0530

    Subarrays of array

--- arrays/subarrays.java
+++ arrays/subarrays.java
@@ -4,20 +4,23 @@ public class subarrays
 {
     public static void sub(int arr[])
     {
+        int ts=0;
         for(int i=0;i<arr.length;i++)
         {
-            int start=i;
+            int start=i; 
             for(int j=i;j<arr.length;j++)
             {
                 int end =j;
                 for(int k=start;k<=end;k++)
                 {
                   System.out.print(arr[k]+" ");
+                  ts++;
                 }
                 System.out.println();
             }
             System.out.println();
-        }           
+        }      
+        System.out.println("total subarrays: "+ts);
     }
     public static void main(String args[])    
     {
