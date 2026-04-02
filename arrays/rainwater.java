9bd97de58a3adea08f73845e4f838e2556a3e804
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-02 10:51:57 +0530

    Trapped Rainwater

--- arrays/rainwater.java
+++ arrays/rainwater.java
@@ -1,39 +1,35 @@
-import java.util.*;
-
-public class rainwater {
-
-    public static int trap(int height[]) {
-        int n = height.length;
-
+public class rainwater
+{
+    public static int trapped(int arr[])
+    {
+        int n=arr.length;
         
-        int leftMax[] = new int[n];
-        leftMax[0] = height[0];
-
-        for(int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        int left[]=new int[n];
+        left[0]=arr[0];
+        for (int i=1;i<n;i++)
+        {
+            left[i]=Math.max(left[i-1],arr[i]);
         }
-
+        
+        int right[]=new int[n];
+        right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=Math.max(right[i+1],arr[i]);
+        }
+        int water=0;
+        for(int i=0;i<n;i++)
+        {
+            int level=Math.min(right[i],left[i]);
+            water+=level-arr[i];
+        }
+        return(water);
+    }
+    public static void main(String args[])
+    {
+       int arr[] ={7,2,5,3,8};
+       System.out.println("Water trapped ="+trapped(arr));
        
-        int rightMax[] = new int[n];
-        rightMax[n-1] = height[n-1];
-
-        for(int i = n-2; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
-        }
-
-        
-        int water = 0;
-
-        for(int i = 0; i < n; i++) {
-            int level = Math.min(leftMax[i], rightMax[i]);
-            water += level - height[i];
-        }
-
-        return water;
     }
-
-    public static void main(String args[]) {
-        int height[] = {4, 2, 0, 6, 3, 2, 5};
-        System.out.println("Trapped Water = " + trap(height));
-    }
+    
 }
