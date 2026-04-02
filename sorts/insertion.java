265e01eb30dd6d4cb29af25d05639e96af4ff624
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-02 19:02:39 +0530

    Insertion sort

--- sorts/insertion.java
+++ sorts/insertion.java
@@ -3,17 +3,17 @@ public class insertion {
         int n = arr.length;
 
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
+            int curr = arr[i];
+            int j = i - 1; //previous
 
-            // shift elements
-            while (j >= 0 && arr[j] > key) {
+            
+            while (j >= 0 && arr[j] > curr) {
                 arr[j + 1] = arr[j];
                 j--;
             }
 
-            // insert element
-            arr[j + 1] = key;
+            
+            arr[j + 1] = curr;
         }
     }
 
