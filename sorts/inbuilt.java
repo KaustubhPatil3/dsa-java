11059c8c9a1c6c3c383ad454adbdbed617ae7d18
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-02 19:09:50 +0530

    inbuilt sorting

---
+++ sorts/inbuilt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class inbuilt 
+{
+    public static void main(String args[])
+    {
+    int arr[]= {2,5,6,7,3};
+    Arrays.sort(arr);
+    for(int num : arr)
+    {
+        System.out.print (num+" ");
+    }
+    }
+}
