364d23825b07f0c8f161cc551365488789d47fa6
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-03 19:59:09 +0530

    Clear range of bits

---
+++ bit_manipulation/clear_range.java
@@ -0,0 +1,14 @@
+public class clear_range
+ {
+    public static int clearRange(int n, int i, int j) {
+        int a = (~0) << (j + 1);
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+
+        return n & bitMask;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(clearRange(31, 1, 3));
+    }
+}
