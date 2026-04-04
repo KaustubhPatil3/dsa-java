9059e27093b0633c991f28fe0486ca318d21d947
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-04 19:41:12 +0530

    1-10 Decreasing order

---
+++ recursion/Decreasing.java
@@ -0,0 +1,15 @@
+public class Decreasing 
+{
+    public static void printInc(int n) 
+    {
+        if (n == 0) return;   
+
+        printInc(n - 1);      
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args)
+    {
+        printInc(10);
+    }
+}
