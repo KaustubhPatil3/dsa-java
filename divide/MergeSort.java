7626b72a2a83bd6eca928d54ebaf6f2b4880ec16
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-06 18:13:26 +0530

    Merge Sort

---
+++ divide/MergeSort.java
@@ -0,0 +1,53 @@
+public class MergeSort {
+
+    public static void mergeSort(int arr[], int si, int ei) 
+    {
+        if (si >= ei) return;  // base case
+
+        int mid = (si + ei) / 2;
+
+        mergeSort(arr, si, mid);      // left
+        mergeSort(arr, mid + 1, ei);  // right
+
+        merge(arr, si, mid, ei);      // merge
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) 
+    {
+        int temp[] = new int[ei - si + 1];
+
+        int i = si;       // left pointer
+        int j = mid + 1;  // right pointer
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6, 3, 9, 5, 2, 8};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
