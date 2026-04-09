94afc70d40ac39f678cb3d8ca14e442d4ccf032b
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-09 18:14:57 +0530

    Multidimensional ArrayList

--- arraylist/Multidimensional.java
+++ arraylist/Multidimensional.java
@@ -19,12 +19,12 @@ public class Multidimensional {
             list3.add(3 * i); // 3,6,9,12,15
         }
 
-        // add to main list
+       
         mainList.add(list1);
         mainList.add(list2);
         mainList.add(list3);
 
-        // print
+        
         for(int i = 0; i < mainList.size(); i++) {
             ArrayList<Integer> currList = mainList.get(i);
 
