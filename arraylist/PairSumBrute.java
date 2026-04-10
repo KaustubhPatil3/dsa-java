e646c03d878799e7a50dabfcb2f9b9f090b9733a
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-10 23:12:43 +0530

    Pair Sum using Brute Force

---
+++ arraylist/PairSumBrute.java
@@ -0,0 +1,16 @@
+public class PairSumBrute {
+    public static void main(String args[]) {
+
+        int arr[] = {2, 4, 6, 8, 10};
+        int target = 10;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+
+                if(arr[i] + arr[j] == target) {
+                    System.out.println("Pair: " + arr[i] + ", " + arr[j]);
+                }
+            }
+        }
+    }
+}
