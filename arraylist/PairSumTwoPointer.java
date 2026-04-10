6efc4ce3d27ea464d65c83bf8198cf164f56ba87
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-10 23:13:58 +0530

    Pair Sum using Two Pointer

---
+++ arraylist/PairSumTwoPointer.java
@@ -0,0 +1,25 @@
+public class PairSumTwoPointer {
+    public static void main(String args[]) {
+
+        int arr[] = {2, 4, 6, 8, 10};
+        int target = 10;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if(sum == target) {
+                System.out.println("Pair: " + arr[left] + ", " + arr[right]);
+                break;
+            }
+            else if(sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+}
