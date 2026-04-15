240f5a988a8872a6cd3bf41e9797beb847be6ba4
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-15 10:59:59 +0530

    Check If Cycle is present in  LL

---
+++ linkedlist/CycleLL.java
@@ -0,0 +1,88 @@
+public class CycleLL
+{
+    public static Node head;
+    public static Node tail;
+
+    public static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    
+    public static void addLast(int data)
+    {
+        Node newNode = new Node(data);
+
+        if(head == null)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public static void printLL()
+    {
+        Node temp = head;
+        int count = 0; 
+
+        while(temp != null && count < 20)
+        {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+            count++;
+        }
+
+        System.out.println("null");
+    }
+
+    
+    public static boolean isCycle()
+    {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;         
+            fast = fast.next.next;    
+
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String args[])
+    {
+        
+        addLast(10);
+        addLast(20);
+        addLast(30);
+        addLast(40);
+
+       
+        tail.next = head.next;  // 40 → 20
+
+        
+        if(isCycle())
+        {
+            System.out.println("Cycle exists");
+        }
+        else
+        {
+            System.out.println("No cycle");
+        }
+    }
+}
