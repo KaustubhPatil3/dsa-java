7e1be36c79a47a0a8cdac0517f23c5cfb6f993b8
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-18 23:37:01 +0530

    Stack Using COllection Framework

---
+++ stack/StackCF.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class StackCF {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(10);
+        s.push(20);
+        System.out.println(s.pop());
+    }
+}
