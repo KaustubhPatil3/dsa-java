c143b6860e6e3874e0132dca7d2ad3bcd6063357
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-19 22:12:44 +0530

    Stack Basics

--- stack/StackIntro.java
+++ stack/StackIntro.java
@@ -1,29 +1,90 @@
-public class StackIntro {
+public class StackIntro
+{
     static int stack[] = new int[5];
     static int top = -1;
 
-    public static void push(int data) {
-        if (top == stack.length - 1) {
-            System.out.println("Overflow");
+    public static boolean isEmpty() 
+    {
+        return top == -1;
+    }
+
+ 
+    public static boolean isFull() 
+    {
+        return top == stack.length - 1;
+    }
+
+    public static void push(int data)
+    {
+        if (isFull()) {
+            System.out.println("Overflow - Cannot push " + data);
             return;
         }
         stack[++top] = data;
+        System.out.println("Pushed: " + data);
     }
 
-    public static int pop() {
-        if (top == -1) {
-            System.out.println("Underflow");
+    public static int pop()
+    {
+        if (isEmpty()) {
+            System.out.println("Underflow - Stack is empty");
             return -1;
         }
-        return stack[top--];
+        int popped = stack[top--];
+        System.out.println("Popped: " + popped);
+        return popped;
     }
 
-    public static int peek() {
+    public static int peek() 
+    {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        System.out.println("Top element: " + stack[top]);
         return stack[top];
     }
 
-    public static void main(String[] args) {
-        push(10); push(20);
-        System.out.println(pop());
+    
+    public static void display()  
+    {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.print("Stack (top -> bottom): ");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(stack[i]);
+            if (i != 0) System.out.print(" -> ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) 
+    {
+        // Push elements
+        push(10);
+        push(20);
+        push(30);
+        push(40);
+        push(50);
+        push(60); 
+
+        System.out.println();
+        display();
+
+        System.out.println();
+        peek();
+
+        System.out.println();
+       
+        pop();
+        pop();
+        display();
+
+        System.out.println();
+       
+        pop(); pop(); pop();
+        pop(); 
     }
 }
