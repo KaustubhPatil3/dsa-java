10800a8f849e261d99240df96cb43f61f3c2f634
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-21 23:47:49 +0530

    Next Greater Element

---
+++ stack/NextGreater.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class NextGreater{
+    public static int[] nextGreater(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>(); 
+
+        
+        for (int i = n - 1; i >= 0; i--) {
+           
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+
+           
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(arr[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 10, 8};
+        int[] result = nextGreater(arr);
+
+        System.out.println("Array           : ");
+        for (int a : arr) System.out.print(a + " ");
+
+        System.out.println("\nNext Greater    : ");
+        for (int r : result) System.out.print(r + " ");
+    }
+}
