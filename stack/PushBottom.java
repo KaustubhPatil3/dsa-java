fb747c47609c1ab7ac7b8e18eb2a092ed1330d72
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-04-26 21:52:18 +0530

    Push At Bottom

--- stack/PushBottom.java
+++ stack/PushBottom.java
@@ -28,8 +28,8 @@ public class PushBottom {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         
         System.out.println("--- Basic pushBottom ---");
         Stack<Integer> s1 = new Stack<>();
@@ -38,40 +38,12 @@ public class PushBottom {
         pushBottom(s1, 99);
         System.out.print("After pushBottom(99): "); display(s1);
 
-        
-        System.out.println("\n--- Single Element ---");
-        Stack<Integer> s2 = new Stack<>();
-        s2.push(10);
-        System.out.print("Before: "); display(s2);
-        pushBottom(s2, 55);
-        System.out.print("After pushBottom(55): "); display(s2);
-
-        
+               
         System.out.println("\n--- Empty Stack ---");
         Stack<Integer> s3 = new Stack<>();
         System.out.print("Before: "); display(s3);
         pushBottom(s3, 77);
         System.out.print("After pushBottom(77): "); display(s3);
-
-       
-        System.out.println("\n--- Multiple pushBottoms ---");
-        Stack<Integer> s4 = new Stack<>();
-        s4.push(10); s4.push(20); s4.push(30);
-        System.out.print("Initial : "); display(s4);
-        pushBottom(s4, 5);
-        System.out.print("pushBottom(5)  : "); display(s4);
-        pushBottom(s4, 2);
-        System.out.print("pushBottom(2)  : "); display(s4);
-        pushBottom(s4, 1);
-        System.out.print("pushBottom(1)  : "); display(s4);
-
-        
-        System.out.println("\n--- Build Stack Bottom-Up ---");
-        Stack<Integer> s5 = new Stack<>();
-        int[] values = {10, 20, 30, 40, 50};
-        for (int v : values) {
-            pushBottom(s5, v);
-            System.out.print("pushBottom(" + v + "): "); display(s5);
-        }
+   
     }
 }
