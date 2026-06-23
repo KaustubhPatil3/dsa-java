20cef5efa15c41d8389fba52a0f3e92752073999
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-06-23 23:39:21 +0530

    Diameter Approach 1

---
+++ binary_tree/DiameterApproach1.java
@@ -0,0 +1,67 @@
+public class DiameterApproach1 {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public Node buildTree(int[] nodes) {
+            idx++;
+
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDiameter = diameter(root.left);
+        int rightDiameter = diameter(root.right);
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        int selfDiameter = leftHeight + rightHeight + 1;
+
+        return Math.max(selfDiameter,
+                Math.max(leftDiameter, rightDiameter));
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+
+        System.out.println(diameter(root));
+    }
+}
