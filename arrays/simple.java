2ab39bb3aa39d319811c3742af2e35dbfc7ffe3a
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-06-30 18:17:47 +0530

    Revised Simple Array Code

--- arrays/simple.java
+++ arrays/simple.java
@@ -1,26 +1,20 @@
-//Pass by reference
-import java.util.*;
-
 public class simple
 {
-     public static void update(int arr[])
+    public static void update(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
         {
-            
-            for(int i=0;i<arr.length;i++)
-            {
-                 arr[i] = arr[i]+1;
-            }
+            arr[i]=arr[i]+1;
         }
-    public static void main(String args[])
-        {
-    
-        int arr[]={10,20,29};
+    }
+    public static void main(String s[])
+    {
+        int arr[]={11,2,3,4};
         update(arr);
-        System.out.println("Array Values :");
-
-            for(int i=0;i<arr.length;i++)
-            {
-                System.out.println(arr[i]+" ");
-            }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(" "+arr[i]);
         }
+    }   
 }
+
