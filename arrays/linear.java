88a527eb0c46027f2ef126820dbb66b7c0982347
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-06-30 19:06:36 +0530

    Revised Linear Search  Array Code

--- arrays/linear.java
+++ arrays/linear.java
@@ -1,35 +1,28 @@
-//Linear search.
-import java.util.*;
-
-public class linear 
+public class Linear
 {
-    public static int line(int arr[],int num)
+    public static int search(int arr[] , int n)
     {
-        int n=num;
-
-        for (int i=0;i<arr.length;i++)
+    
+        int pos= 0;
+        for(int i=0;i<arr.length;i++)
         {
-            if (arr[i]==n)
+            if(arr[i]==n)
             {
-               return i; 
+                 pos =i;
             }
         }
-        return -1;
-
+        return pos;
     }
-
-    public static void main(String args[])
+    public static void main(String s[])
     {
-        int arr[]={2,5,3,8,7,1};
-        int num=7;
-        int i=line(arr,num);
-        if(i==-1)
+        int arr[]={1,4,2,8,5,9,3};
+        int num =5;
+        int i= search(arr,num);
+        if(i==0)
         {
             System.out.println("Not Found");
         }
         else
-        {
-            System.out.println("Number found at="+i);
-        }
+            System.out.println("Found " +arr[i] +" at "+i+" position");
     }
 }
