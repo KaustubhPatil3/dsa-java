24d68655dce2434f0cc1733ac8569e79cfe3e22a
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-07-01 19:38:12 +0530

    Revised Array Pairs Code

--- arrays/pairs.java
+++ arrays/pairs.java
@@ -1,26 +1,18 @@
-import java.util.*;
-
-public class pairs 
+public class Pairs 
 {
-    public static void make(int arr[])
+    public static void pair(int arr[])
     {
         for(int i=0;i<arr.length;i++)
         {
-            int current=arr[i];
-
             for(int j=i+1;j<arr.length;j++)
             {
-                System.out.print("("+current+","+arr[j]+")");
+                System.out.print("("+arr[i]+","+arr[j]+")");
             }
-            System.out.println();
-        }   
-        
-        
+        }
     }
-    public static void main(String args[])    
+    public static void main(String s[])
     {
-        int num[]={2,3,4,5,6};
-
-        make(num);
+        int arr[]={1,2,3,4,5};
+        pair(arr);
     }
 }
