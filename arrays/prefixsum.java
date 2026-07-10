6c717c9cf90621e2b8e732ee6f6bb7adb64b533a
Kaustubh Patil <kaustubhpatil.sknsits.comp@gmail.com>
2026-07-10 22:42:34 +0530

    Prefix Sum

--- arrays/prefixsum.java
+++ arrays/prefixsum.java
@@ -1,52 +1,46 @@
-import java.util.*;
-
-public class prefixsum
+public class Prefixsum 
 {
-    public static void sub(int arr[])
+    public static void pre(int arr[])
     {
-        int n = arr.length;
+        int n=arr.length;
+        int prefix[]=new int[n];
+        prefix[0]=arr[0];
 
-        int prefix[] = new int[n];
-        prefix[0] = arr[0];
-
-        for(int i = 1; i < n; i++)
+        for (int i=1;i<n;i++)
         {
-            prefix[i] = prefix[i-1] + arr[i];
+            prefix[i]=prefix[i-1]+arr[i];
         }
 
-        int max = Integer.MIN_VALUE;
-       
-        
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i; j < n; j++)
-            {
-                int sum;
+        int max=Integer.MIN_VALUE;
 
-                if(i == 0)
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                int sum=0;
+
+                if(i==0)
                 {
-                    sum = prefix[j];
+                    sum =arr[j];
                 }
+
                 else
                 {
-                    sum = prefix[j] - prefix[i-1];
+                    sum =prefix[j]-prefix[i-1];
                 }
 
-               
-
-              
-                if(sum > max)
+                if(sum>max)
                 {
-                    max = sum;
+                    max=sum;
                 }
             }
         }
-        System.out.println("Maximum sum: " + max);  
+            System.out.println("Sum="+max);
     }
-
-    public static void main(String args[])    
+    public static void main(String s[])
     {
-        int num[] = {2, 3, 4, 5, 6};
-        sub(num);
+        int a[]={1,2,3,4,5};
+        pre(a);
     }
 }
+
